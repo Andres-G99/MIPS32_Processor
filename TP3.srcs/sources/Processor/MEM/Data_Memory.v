bc34fa2b57a3041c39aedeef98f5e394201bf4f0
Andres-G99 <andres_garella_99@hotmail.com>
2024-05-12 20:17:41 -0300

    Data Memory

---
+++ TP3.srcs/sources/Processor/MEM/Data_Memory.v
@@ -0,0 +1,43 @@
+`timescale 1ns / 1ps
+
+module Data_Memory
+    #(
+        parameter ADDRESS_WIDTH = 5, // 32 slots de memoria
+        parameter MEM_SLOT_WIDTH = 8 // Cada slot de memoria tiene 8 bits
+    )
+    (
+        input wire clk,
+        input wire i_reset, // reset de la memoria
+        input wire i_wr_rd, // 0: read, 1: write
+        input wire [ADDRESS_WIDTH-1:0] i_address, // direccion de memoria
+        input wire [MEM_SLOT_WIDTH-1:0] i_data, // dato a escribir
+        output wire [MEM_SLOT_WIDTH-1:0] o_data // dato leido
+        // linea para el debug
+
+    );
+
+    reg [MEM_SLOT_WIDTH-1:0] mem [0:2**ADDRESS_WIDTH-1]; // Memoria de 32 slots de 8 bits
+    // mem es una memoria con 2**ADDRESS_WIDTH slots (32) de MEM_SLOT_WIDTH bits (8 bits)
+
+    integer i = 0; // Iterador
+    always @(posedge clk) 
+    begin
+        if (i_reset) 
+            begin
+                for (i = 0; i < 2**ADDRESS_WIDTH; i = i+1)
+                    mem[i] <= 8'b0; // Limpia la memoria
+            end
+        else
+            begin
+                if (i_wr_rd) // Write
+                    mem[i_address] <= i_data; // Escribe en la direccion i_address el dato i_data
+            end
+    end
+
+    assign o_data = mem[i_address]; // Lee el dato en la direccion i_address
+
+    /*
+        Debugger code
+    */
+
+endmodule
---
+++ TP3.srcs/sources/Processor/MEM/MEM_Unit.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12.05.2024 19:48:01
+// Design Name: 
+// Module Name: MEM_Unit
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module MEM_Unit(
+
+    );
+endmodule
