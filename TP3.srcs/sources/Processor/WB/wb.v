3543e29d5cefcb4167fae41da89c10c5818c283b
Camila-personal <careggiocamila@gmail.com>
2024-08-11 12:03:12 -0300

    comments WB stage

--- TP3.srcs/sources/Processor/WB/wb.v
+++ TP3.srcs/sources/Processor/WB/wb.v
@@ -1,16 +1,22 @@
 `timescale 1ns / 1ps
 
+/*
+Se implementa la etapa de Write Back (WB),
+que escribe en los registros el resultado correspondiente.
+*/
+
 module wb
     #(
         parameter BUS_WIDTH = 32
     )
     (
-        input  wire i_mem_to_reg,
+        input  wire i_mem_to_reg, // indica si hay que escribir en los registros
         input  wire [BUS_WIDTH - 1 : 0] i_alu_result,
         input  wire [BUS_WIDTH - 1 : 0] i_mem_result,
-        output wire [BUS_WIDTH - 1 : 0] o_wb_data
+        output wire [BUS_WIDTH - 1 : 0] o_wb_data  // datos a escribir en los registros
     );
 
+    /* Seleccionar si escribimos el resultado de la ALU o el de la opp en memoria */
     mux 
     #(
         .CHANNELS (2), 
